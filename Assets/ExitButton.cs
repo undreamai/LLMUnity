31012aaee6dee1d47ec99a588966247053097b76
Antonis Makropoulos <benuix@gmail.com>
2023-12-12 12:17:09 +0200

    add exit button

---
+++ Assets/ExitButton.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        // This method will be called when the button is clicked
+        Debug.Log("Exit button clicked");
+        Application.Quit();
+    }
+}
