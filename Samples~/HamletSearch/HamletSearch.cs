e5f82b54bdb05c7124d4dd8d35593fbb0753ccd9
Antonis Makropoulos <benuix@gmail.com>
2024-02-02 19:01:05 +0200

    add metrics

--- Samples~/HamletSearch/HamletSearch.cs
+++ Samples~/HamletSearch/HamletSearch.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using Debug = UnityEngine.Debug;
 using System.Text.RegularExpressions;
 using LLMUnity;
+using System.Linq;
 
 class HamletSearch : MonoBehaviour
 {
@@ -14,32 +15,39 @@ class HamletSearch : MonoBehaviour
 
     public void OnEnable()
     {
-        embedder = new BGEModel("Assets/StreamingAssets/bge-small-en-v1.5.sentis", "Assets/StreamingAssets/bge-small-en-v1.5.tokenizer.json");
+        embedder = new BGEModel(
+            "Assets/StreamingAssets/bge-small-en-v1.5.sentis",
+            "Assets/StreamingAssets/bge-small-en-v1.5.tokenizer.json"
+        );
     }
 
     void Start()
     {
         List<Dialogue> dialogues = ReadGutenbergFile(gutenbergText.text);
+        List<Dialogue> dialoguesToDo = fullPlay ? dialogues : new List<Dialogue> { dialogues[2] };
         search = new ANNModelSearch(embedder);
 
         Stopwatch stopwatch = new Stopwatch();
         float elapsed_total = 0;
+        int numSentences = 0;
 
-        List<Dialogue> dialoguesToDo = fullPlay ? dialogues : new List<Dialogue> { dialogues[2] };
         foreach (Dialogue dialogue in dialoguesToDo)
         {
+            string[] sentences = dialogue.GetSentences().ToArray();
+            numSentences += sentences.Length;
+
             stopwatch.Reset(); stopwatch.Start();
-            search.Add(dialogue.GetSentences().ToArray());
+            search.Add(sentences);
             stopwatch.Stop();
 
             float elapsed = (float)stopwatch.Elapsed.TotalMilliseconds / 1000f;
-            Debug.Log($"{dialogue.Title} embed time: {elapsed} secs");
             elapsed_total += elapsed;
+            Debug.Log($"{dialogue.Title} embed time: {elapsed} secs");
         }
-        Debug.Log($"embedded {search.Count()} sentences in {elapsed_total} secs");
+        Debug.Log($"embedded {numSentences} sentences in {elapsed_total} secs");
 
         stopwatch.Reset(); stopwatch.Start();
-        List<string> similar = search.RetrieveSimilar("should i exist?", 5);
+        List<string> similar = search.RetrieveSimilar("should i exist?", 10);
         stopwatch.Stop();
         Debug.Log($"search time: {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
 
@@ -62,6 +70,8 @@ class HamletSearch : MonoBehaviour
         string message = "";
         bool add = false;
         Dialogue dialogue = null;
+        int numWords = 0;
+        int numLines = 0;
 
         string[] lines = text.Split("\n");
         for (int i = 0; i < lines.Length; i++)
@@ -74,6 +84,8 @@ class HamletSearch : MonoBehaviour
             line = line.Replace("\r", "");
             line = Regex.Replace(line, skipPattern, "");
             if (line == "") continue;
+            numWords += line.Split(new[] { ' ', '\t' }, System.StringSplitOptions.RemoveEmptyEntries).Length;
+            numLines++;
 
             if (line.StartsWith("ACT"))
             {
@@ -101,8 +113,8 @@ class HamletSearch : MonoBehaviour
                 if (message != "") message += " ";
                 message += line;
             }
-
         }
+        Debug.Log($"{numLines} lines, {numWords} words");
         return dialogues;
     }
 
