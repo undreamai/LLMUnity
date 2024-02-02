9ea01cf32cf5234c2feb5e7bb962ef5122e8acf6
Antonis Makropoulos <benuix@gmail.com>
2024-02-02 16:31:36 +0200

    add namespace, implement Count method

--- Runtime/Search.cs
+++ Runtime/Search.cs
@@ -4,123 +4,137 @@ using System.Linq;
 using Unity.Sentis;
 using Cloud.Unum.USearch;
 
-public abstract class Search<T, E> where E : Embedder<T>
+
+namespace LLMUnity
 {
-    protected E embedder;
-    protected Dictionary<string, T> embeddings;
-
-    public Search(E embedder)
+    public abstract class Search<T, E> where E : Embedder<T>
     {
-        embeddings = new Dictionary<string, T>();
-        this.embedder = embedder;
-    }
+        protected E embedder;
+        protected Dictionary<string, T> embeddings;
 
-    public abstract void Insert(string inputString, T encoding);
+        public abstract void Insert(string inputString, T encoding);
+        public abstract List<string> RetrieveSimilar(string queryString, int k);
 
-    public T Add(string inputString)
-    {
-        T embedding = embedder.Encode(inputString);
-        Insert(inputString, embedding);
-        return embedding;
-    }
-
-    public T[] Add(string[] inputStrings)
-    {
-        T[] inputEmbeddings = new T[inputStrings.Length];
-        for (int i = 0; i < inputStrings.Length; i++)
+        public Search(E embedder)
         {
-            inputEmbeddings[i] = Add(inputStrings[i]);
+            embeddings = new Dictionary<string, T>();
+            this.embedder = embedder;
         }
-        return inputEmbeddings;
-    }
 
-    public List<string> GetKNN(string[] inputStrings, float[] scores, int k = -1)
-    {
-        var sortedLists = inputStrings.Zip(scores, (first, second) => new { First = first, Second = second })
-            .OrderByDescending(item => item.Second)
-            .ToList();
-        List<string> results = new List<string>();
-        int kmax = k == -1 ? sortedLists.Count : Math.Min(k, sortedLists.Count);
-        for (int i = 0; i < kmax; i++)
+        public T Add(string inputString)
         {
-            results.Add(sortedLists[i].First);
+            T embedding = embedder.Encode(inputString);
+            Insert(inputString, embedding);
+            return embedding;
         }
-        return results;
-    }
 
-    public abstract List<string> RetrieveSimilar(string queryString, int k);
-}
-
-public class ModelSearch : Search<TensorFloat, EmbeddingModel>
-{
-    public ModelSearch(EmbeddingModel embedder) : base(embedder) {}
-
-    public override void Insert(string inputString, TensorFloat encoding){
-        embeddings[inputString] = encoding;
-    }
-
-    public new TensorFloat[] Add(string[] inputStrings)
-    {
-        TensorFloat[] inputEmbeddings = (TensorFloat[])embedder.Split(embedder.Encode(inputStrings));
-        for (int i = 0; i < inputStrings.Length; i++)
+        public T[] Add(string[] inputStrings)
         {
-            Insert(inputStrings[i], inputEmbeddings[i]);
+            T[] inputEmbeddings = new T[inputStrings.Length];
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                inputEmbeddings[i] = Add(inputStrings[i]);
+            }
+            return inputEmbeddings;
         }
-        return inputEmbeddings;
-    }
 
-    public override List<string> RetrieveSimilar(string queryString, int k)
-    {
-        TensorFloat queryEmbedding = embedder.Encode(queryString);
-        TensorFloat storeEmbedding = embedder.Concat(embeddings.Values.ToArray());
-        float[] scores = embedder.SimilarityScores(queryEmbedding, storeEmbedding);
-        return GetKNN(embeddings.Keys.ToArray(), scores, k);
-    }
-}
-
-public class ANNModelSearch : ModelSearch
-{
-    USearchIndex index;
-    protected Dictionary<string, ulong> TextToUSearch;
-    protected Dictionary<ulong, string> USearchToText;
-    ulong nextKey;
-
-    public ANNModelSearch(
-        EmbeddingModel embedder,
-        ulong dimensions,
-        MetricKind metricKind = MetricKind.Cos,
-        ulong connectivity = 32,
-        ulong expansionAdd = 40,
-        ulong expansionSearch = 16,
-        bool multi = false
-    ) : this(embedder, new USearchIndex(dimensions, metricKind, connectivity, expansionAdd, expansionSearch, multi)) {}
-
-    public ANNModelSearch(
-        EmbeddingModel embedder,
-        USearchIndex index
-    ) : base(embedder) {
-        this.index = index;
-        TextToUSearch = new Dictionary<string, ulong>();
-        USearchToText = new Dictionary<ulong, string>();
-        nextKey = 0;
-    }
-
-    public override void Insert(string inputString, TensorFloat encoding){
-        if (TextToUSearch.ContainsKey(inputString)) return;
-        ulong key = nextKey++;
-        TextToUSearch[inputString] = key;
-        USearchToText[key] = inputString;
-        index.Add(key, encoding.ToReadOnlyArray());
-    }
-
-    public override List<string> RetrieveSimilar(string queryString, int k)
-    {
-        float[] queryEmbedding = embedder.Encode(queryString).ToReadOnlyArray();
-        index.Search(queryEmbedding, k, out ulong[] keys, out float[] distances);
-        List<string> result = new List<string>();
-        for(int i=0; i<keys.Length; i++){
-            result.Add(USearchToText[keys[i]]);
+        public List<string> GetKNN(string[] inputStrings, float[] scores, int k = -1)
+        {
+            var sortedLists = inputStrings.Zip(scores, (first, second) => new { First = first, Second = second })
+                .OrderByDescending(item => item.Second)
+                .ToList();
+            List<string> results = new List<string>();
+            int kmax = k == -1 ? sortedLists.Count : Math.Min(k, sortedLists.Count);
+            for (int i = 0; i < kmax; i++)
+            {
+                results.Add(sortedLists[i].First);
+            }
+            return results;
         }
-        return result;
+
+        public int Count()
+        {
+            return embeddings.Count;
+        }
+    }
+
+    public class ModelSearch : Search<TensorFloat, EmbeddingModel>
+    {
+        public ModelSearch(EmbeddingModel embedder) : base(embedder) {}
+
+        public override void Insert(string inputString, TensorFloat encoding){
+            embeddings[inputString] = encoding;
+        }
+
+        public new TensorFloat[] Add(string[] inputStrings)
+        {
+            TensorFloat[] inputEmbeddings = (TensorFloat[])embedder.Split(embedder.Encode(inputStrings));
+            for (int i = 0; i < inputStrings.Length; i++)
+            {
+                Insert(inputStrings[i], inputEmbeddings[i]);
+            }
+            return inputEmbeddings;
+        }
+
+        public override List<string> RetrieveSimilar(string queryString, int k)
+        {
+            TensorFloat queryEmbedding = embedder.Encode(queryString);
+            TensorFloat storeEmbedding = embedder.Concat(embeddings.Values.ToArray());
+            float[] scores = embedder.SimilarityScores(queryEmbedding, storeEmbedding);
+            return GetKNN(embeddings.Keys.ToArray(), scores, k);
+        }
+    }
+
+    public class ANNModelSearch : ModelSearch
+    {
+        USearchIndex index;
+        protected Dictionary<string, ulong> TextToUSearch;
+        protected Dictionary<ulong, string> USearchToText;
+        ulong nextKey;
+
+        public ANNModelSearch(
+            EmbeddingModel embedder,
+            ulong dimensions,
+            MetricKind metricKind = MetricKind.Cos,
+            ulong connectivity = 32,
+            ulong expansionAdd = 40,
+            ulong expansionSearch = 16,
+            bool multi = false
+        ) : this(embedder, new USearchIndex(dimensions, metricKind, connectivity, expansionAdd, expansionSearch, multi)) {}
+
+        public ANNModelSearch(
+            EmbeddingModel embedder,
+            USearchIndex index
+        ) : base(embedder) {
+            this.index = index;
+            TextToUSearch = new Dictionary<string, ulong>();
+            USearchToText = new Dictionary<ulong, string>();
+            nextKey = 0;
+        }
+
+        public override void Insert(string inputString, TensorFloat encoding){
+            if (TextToUSearch.ContainsKey(inputString)) return;
+            ulong key = nextKey++;
+            TextToUSearch[inputString] = key;
+            USearchToText[key] = inputString;
+            index.Add(key, encoding.ToReadOnlyArray());
+        }
+
+        public override List<string> RetrieveSimilar(string queryString, int k)
+        {
+            float[] queryEmbedding = embedder.Encode(queryString).ToReadOnlyArray();
+            index.Search(queryEmbedding, k, out ulong[] keys, out float[] distances);
+            List<string> result = new List<string>();
+            for(int i=0; i<keys.Length; i++){
+                result.Add(USearchToText[keys[i]]);
+            }
+            return result;
+        }
+
+        public new int Count()
+        {
+            Console.Write(index.Size());
+            return (int) index.Size();
+        }
     }
 }
