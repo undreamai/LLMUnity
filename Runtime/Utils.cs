44bd488e661bacce29d52157354b6cbc0f229e60
Antonis Makropoulos <benuix@gmail.com>
2024-02-08 08:45:07 +0200

    add saver Utils

---
+++ Runtime/Utils.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Json;
+
+namespace LLMUnity
+{
+    public class Saver
+    {
+        public static void Save(object saveObject, string filePath, string name, FileMode mode = FileMode.Create)
+        {
+            using (FileStream stream = new FileStream(filePath, mode))
+            using (ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Create))
+            {
+                Save(saveObject, archive, name);
+            }
+        }
+
+        public static void Save(object saveObject, ZipArchive archive, string name)
+        {
+            ZipArchiveEntry mainEntry = archive.CreateEntry(name);
+            using (Stream entryStream = mainEntry.Open())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(saveObject.GetType());
+                serializer.WriteObject(entryStream, saveObject);
+            }
+        }
+
+        public static T Load<T>(string filePath, string name)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Open))
+            using (ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Read))
+            {
+                return Load<T>(archive, name);
+            }
+        }
+
+        public static T Load<T>(ZipArchive archive, string name)
+        {
+            ZipArchiveEntry baseEntry = archive.GetEntry(name);
+            using (Stream entryStream = baseEntry.Open())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                T obj = (T)serializer.ReadObject(entryStream);
+                return obj;
+            }
+        }
+
+        public static string EscapeFileName(string fileName)
+        {
+            foreach (char invalidChar in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(invalidChar, '_');
+            }
+            return fileName;
+        }
+
+        public static string RemoveExtension(string name)
+        {
+            return Path.Combine(Path.GetDirectoryName(name), Path.GetFileNameWithoutExtension(name));
+        }
+    }
+}
