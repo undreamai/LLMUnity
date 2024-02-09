1d0468585f0b09c11bfdd5bbcbddc523451b1aa2
Antonis Makropoulos <benuix@gmail.com>
2024-02-09 20:54:44 +0200

    add tests for dialogue manager

--- Tests/Runtime/TestDialogue.cs
+++ Tests/Runtime/TestDialogue.cs
@@ -3,6 +3,7 @@ using LLMUnity;
 using System;
 using System.IO;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace LLMUnityTests
 {
@@ -79,6 +80,7 @@ namespace LLMUnityTests
 
         }
     }
+
     public class TestDialogue
     {
         EmbeddingModel model;
@@ -234,6 +236,123 @@ namespace LLMUnityTests
             Assert.AreEqual(distances.Length, 2);
             Assert.That(ApproxEqual(distances[0], 0));
             Assert.That(ApproxEqual(distances[1], 1 - trueSimilarity));
+        }
+    }
+
+    public class TestDialogueManager
+    {
+        EmbeddingModel model;
+        string modelPath;
+        string tokenizerPath;
+        List<(string, string, string)> phrases = new List<(string, string, string)>(){
+            ("Hamlet", "ACT I", "To be, or not to be, that is the question. Whether tis nobler in the mind to suffer."),
+            ("Hamlet", "ACT I", "Or to take arms against a sea of troubles, and by opposing end them? To die—to sleep."),
+            ("Hamlet", "ACT II", "I humbly thank you; well, well, well."),
+            ("Ophelia", "ACT II", "Good my lord."),
+            ("Ophelia", "ACT II", "How does your honour for this many a day?")
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            modelPath = Path.Combine(Application.streamingAssetsPath, "bge-small-en-v1.5.sentis");
+            tokenizerPath = Path.Combine(Application.streamingAssetsPath, "bge-small-en-v1.5.tokenizer.json");
+            model = new EmbeddingModel(modelPath, tokenizerPath, Unity.Sentis.BackendType.CPU, "sentence_embedding", false, 384);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            model.Destroy();
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            DialogueManager manager = new DialogueManager(model);
+            foreach (var phrase in phrases)
+                manager.Add(phrase.Item1, phrase.Item2, phrase.Item3);
+            Assert.AreEqual(manager.NumPhrases(), 5);
+            Assert.AreEqual(manager.NumSentences(), 8);
+            Assert.AreEqual(manager.NumPhrases("Hamlet"), 3);
+            Assert.AreEqual(manager.NumSentences("Hamlet"), 6);
+            Assert.AreEqual(manager.NumPhrases("Ophelia"), 2);
+            Assert.AreEqual(manager.NumSentences("Ophelia"), 2);
+            Assert.AreEqual(manager.NumPhrases(null, "ACT I"), 2);
+            Assert.AreEqual(manager.NumSentences(null, "ACT I"), 4);
+            Assert.AreEqual(manager.NumPhrases(null, "ACT II"), 3);
+            Assert.AreEqual(manager.NumSentences(null, "ACT II"), 4);
+            Assert.AreEqual(manager.NumPhrases("Hamlet", "ACT I"), 2);
+            Assert.AreEqual(manager.NumSentences("Hamlet", "ACT II"), 2);
+
+            Assert.AreEqual(manager.GetPhrases("Hamlet", "ACT II"), new string[] { phrases[2].Item3 });
+            string[] sentencesGT = phrases[2].Item3.Split(";");
+            sentencesGT[0] += ";";
+            sentencesGT[1] = sentencesGT[1].Trim();
+            Assert.AreEqual(manager.GetSentences("Hamlet", "ACT II"), sentencesGT);
+
+            manager.Add(phrases[3].Item1, phrases[3].Item2, phrases[3].Item3);
+            Assert.AreEqual(manager.NumPhrases("Ophelia"), 3);
+            Assert.AreEqual(manager.NumSentences("Ophelia"), 3);
+            manager.Remove(null, null, phrases[2].Item3);
+            Assert.AreEqual(manager.NumPhrases("Hamlet"), 2);
+            Assert.AreEqual(manager.NumSentences("Hamlet"), 4);
+            manager.Remove(null, null, phrases[3].Item3);
+            Assert.AreEqual(manager.NumPhrases("Ophelia"), 1);
+            Assert.AreEqual(manager.NumSentences("Ophelia"), 1);
+            manager.Add("Ophelia", phrases[0].Item2, phrases[0].Item3);
+            manager.Remove("Hamlet", null, phrases[0].Item3);
+            Assert.AreEqual(manager.NumPhrases("Ophelia"), 2);
+            Assert.AreEqual(manager.NumSentences("Ophelia"), 3);
+            Assert.AreEqual(manager.NumPhrases("Hamlet"), 1);
+            Assert.AreEqual(manager.NumSentences("Hamlet"), 2);
+
+            sentencesGT = phrases[1].Item3.Split("?");
+            sentencesGT[0] += "?";
+            sentencesGT[1] = sentencesGT[1].Trim();
+            Assert.AreEqual(manager.GetSentences("Hamlet"), sentencesGT);
+            Assert.AreEqual(manager.GetPhrases("Ophelia"), new string[] { phrases[4].Item3, phrases[0].Item3 });
+        }
+
+        [Test]
+        public void TestSaveLoad()
+        {
+            string path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            DialogueManager manager = new DialogueManager(model);
+            manager.Save(path);
+            DialogueManager loadedManager = DialogueManager.Load(path);
+
+            foreach (var phrase in phrases)
+                manager.Add(phrase.Item1, phrase.Item2, phrase.Item3);
+            manager.Save(path);
+            loadedManager = DialogueManager.Load(path);
+            File.Delete(path);
+
+            Assert.AreEqual(manager.NumSentences(), loadedManager.NumSentences());
+            Assert.AreEqual(manager.NumPhrases(), loadedManager.NumPhrases());
+
+            manager.Remove(null, null, phrases[2].Item3);
+            Assert.AreEqual(manager.NumPhrases("Hamlet"), 2);
+            Assert.AreEqual(manager.NumSentences("Hamlet"), 4);
+        }
+
+        [Test]
+        public void TestSearch()
+        {
+            DialogueManager manager = new DialogueManager(model);
+            foreach (var phrase in phrases)
+                manager.Add(phrase.Item1, phrase.Item2, phrase.Item3);
+            manager.Add("Ophelia", phrases[0].Item2, phrases[0].Item3);
+
+            string[] results = manager.SearchPhrases(phrases[0].Item3, 2);
+            Assert.AreEqual(results, new string[] { phrases[0].Item3, phrases[0].Item3 });
+
+            results = manager.SearchPhrases(phrases[0].Item3, 2, "Hamlet");
+            Assert.AreEqual(results[0], phrases[0].Item3);
+            Assert.AreNotEqual(results[1], phrases[0].Item3);
+            results = manager.SearchPhrases(phrases[1].Item3, 1, "Ophelia");
+            Assert.AreNotEqual(results[0], phrases[1].Item3);
         }
     }
 }
