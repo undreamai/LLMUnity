2ea2f491181d3f71183f257db7f898176bbbe5ee
Antonis Makropoulos <benuix@gmail.com>
2024-02-12 18:28:38 +0200

    text interaction with hamlet

--- Samples~/HamletSearch/HamletSearch.cs
+++ Samples~/HamletSearch/HamletSearch.cs
@@ -4,56 +4,100 @@ using System.Diagnostics;
 using Debug = UnityEngine.Debug;
 using System.Text.RegularExpressions;
 using LLMUnity;
+using System.IO;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 class HamletSearch : MonoBehaviour
 {
+    public InputField playerText;
+    public Text AIText;
     public Embedding embedding;
-    public bool fullPlay;
     public TextAsset gutenbergText;
+
+    Dialogue dialogue;
 
     void Start()
     {
+        StartCoroutine(InitDialogue());
+    }
+
+    IEnumerator<string> InitDialogue()
+    {
+        playerText.interactable = false;
+
         EmbeddingModel model = embedding.GetModel();
         if (model == null)
         {
             throw new System.Exception("Please select an embedding model in the HamletSearch GameObject!");
         }
 
-        Dictionary<string, List<(string, string)>> hamlet = ReadGutenbergFile(gutenbergText.text);
-        Dialogue dialogueManager = new Dialogue(model);
-        Stopwatch stopwatch = new Stopwatch();
+        string sampleDir = Path.GetDirectoryName(Directory.GetFiles(Application.dataPath, "HamletSearch.cs", SearchOption.AllDirectories)[0]);
+        string filename = Path.Combine(sampleDir, "embeddings.zip");
+        if (File.Exists(filename))
+        {
+            playerText.text = "Loading dialogues...";
+            yield return null;
+            dialogue = Dialogue.Load(filename);
+        }
+        else
+        {
+            playerText.text = "Creating embeddings...";
+            yield return null;
+            dialogue = CreateEmbeddings(filename);
+        }
 
-        float elapsedTotal = 0;
+        playerText.interactable = true;
+        InitPlayerText();
+    }
+
+    void InitPlayerText()
+    {
+        playerText.onSubmit.AddListener(onInputFieldSubmit);
+        playerText.onValueChanged.AddListener(onValueChanged);
+        playerText.Select();
+        playerText.text = "";
+    }
+
+    void onValueChanged(string newText)
+    {
+        // Get rid of newline character added when we press enter
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if (playerText.text.Trim() == "")
+                playerText.text = "";
+        }
+    }
+
+    void onInputFieldSubmit(string message)
+    {
+        playerText.interactable = false;
+        AIText.text = dialogue.Search(message)[0];
+        playerText.interactable = true;
+        playerText.Select();
+        playerText.text = "";
+    }
+
+    Dialogue CreateEmbeddings(string filename)
+    {
+        Dialogue dialogue;
+
+        Dictionary<string, List<(string, string)>> hamlet = ReadGutenbergFile(gutenbergText.text);
+        dialogue = new Dialogue(embedding.GetModel());
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         foreach ((string act, List<(string, string)> messages) in hamlet)
         {
-            if (!fullPlay && act != "ACT III") continue;
-            stopwatch.Reset(); stopwatch.Start();
             foreach ((string actor, string message) in messages)
-                dialogueManager.Add(actor, message, act);
-
-            elapsedTotal += (float)stopwatch.Elapsed.TotalMilliseconds / 1000f;
-            Debug.Log($"act {act} embedded {dialogueManager.GetSentences(null, act).Length} sentences in {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
+            {
+                if (actor == "HAMLET") dialogue.Add(message);
+            }
         }
-        Debug.Log($"embedded {dialogueManager.NumPhrases()} phrases, {dialogueManager.NumSentences()} sentences in {elapsedTotal} secs");
+        Debug.Log($"embedded {dialogue.NumPhrases()} phrases, {dialogue.NumSentences()} sentences in {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
 
-        stopwatch.Reset(); stopwatch.Start();
-        dialogueManager.Save("test.zip");
-        Debug.Log($"saving took {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
-
-        stopwatch.Reset(); stopwatch.Start();
-        dialogueManager = Dialogue.Load("test.zip");
-        Debug.Log($"loading took {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
-
-        stopwatch.Reset(); stopwatch.Start();
-        string[] similar = dialogueManager.Search("should i be?", 10);
-        stopwatch.Stop();
-        Debug.Log($"search time: {stopwatch.Elapsed.TotalMilliseconds / 1000f} secs");
-
-        Debug.Log("Similar sentences:");
-        for (int i = 0; i < similar.Length; i++)
-        {
-            Debug.Log($"  {i + 1}: {similar[i]}");
-        }
+        dialogue.Save(filename);
+        return dialogue;
     }
 
     public Dictionary<string, List<(string, string)>> ReadGutenbergFile(string text)
