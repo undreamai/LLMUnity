982f396c28b4cb83d9d1437d6301508b73f01364
Antonis Makropoulos <benuix@gmail.com>
2024-02-27 16:08:09 +0200

    functionality to figure out template from model

--- Runtime/LLMChatTemplates.cs
+++ Runtime/LLMChatTemplates.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 namespace LLMUnity
 {
@@ -6,6 +8,77 @@ namespace LLMUnity
     {
         public readonly List<string> stop;
         public abstract string ComputePrompt(List<ChatMessage> messages);
+
+        public static Dictionary<string, ChatTemplate> templates = new Dictionary<string, ChatTemplate>()
+        {
+            {"chatml", new ChatMLTemplate()},
+            {"mistral", new MistralInstructTemplate()},
+            {"llama", new LLama2Template()},
+            {"alpaca", new AlpacaTemplate()},
+            {"phi", new Phi2Template()},
+            {"zephyr", new ZephyrTemplate()},
+        };
+
+        public static Dictionary<string, string> aliasTemplates = new Dictionary<string, string>()
+        {
+            {"openhermes", "chatml"}
+        };
+
+        public static Dictionary<string, string> chatTemplates = new Dictionary<string, string>()
+        {
+            {"{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}", "chatml"},
+            {"{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token}}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}", "mistral"},
+            {"{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}", "zephyr"}
+        };
+
+        public static string FromName(string name)
+        {
+            string nameLower = name.ToLower();
+            foreach (var pair in aliasTemplates)
+            {
+                if (nameLower.Contains(pair.Key)) return pair.Value;
+            }
+            foreach (var pair in templates)
+            {
+                if (nameLower.Contains(pair.Key)) return pair.Key;
+            }
+            return null;
+        }
+
+        public static string FromTemplate(string template)
+        {
+            string templateTrim = template.Trim();
+            if (chatTemplates.TryGetValue(templateTrim, out string value))
+                return value;
+            return null;
+        }
+
+        public static ChatTemplate FromGGUF(string path, out string name)
+        {
+            GGUFReader reader = new GGUFReader(path, "r");
+            ReaderField field;
+
+            if (reader.fields.TryGetValue("tokenizer.chat_template", out field))
+            {
+                string template = System.Text.Encoding.UTF8.GetString((byte[])field.parts[field.parts.Count - 1]);
+                name = FromTemplate(template);
+                if (name != null) return templates[name];
+            }
+            if (reader.fields.TryGetValue("general.name", out field))
+            {
+                string modelName = System.Text.Encoding.UTF8.GetString((byte[])field.parts[field.parts.Count - 1]);
+                name = FromName(modelName);
+                if (name != null) return templates[name];
+            }
+
+            string filename = Path.GetFileNameWithoutExtension(path);
+            name = FromName(filename);
+            if (name != null) return templates[name];
+
+            Debug.Log("No chat template could be matched, fallback to ChatML");
+            name = "chatml";
+            return templates[name];
+        }
     }
 
     public class ChatMLTemplate : ChatTemplate
