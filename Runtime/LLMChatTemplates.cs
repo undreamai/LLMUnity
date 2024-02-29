33a6691a940dac8f68e7901d1d27b512669bee58
Antonis Makropoulos <benuix@gmail.com>
2024-02-29 09:45:49 +0200

    introduce customisable chat template

--- Runtime/LLMChatTemplates.cs
+++ Runtime/LLMChatTemplates.cs
@@ -1,52 +1,58 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace LLMUnity
 {
     public abstract class ChatTemplate
     {
-        public List<string> stop;
-        public abstract void SetStop();
-        public abstract string ComputePrompt(List<ChatMessage> messages);
+        public string playerName;
+        public string AIName;
 
-        public ChatTemplate()
+        public ChatTemplate(string playerName = "user", string AIName = "assistant")
         {
-            SetStop();
+            this.playerName = playerName;
+            this.AIName = AIName;
         }
 
-        public static Dictionary<string, ChatTemplate> templates = new Dictionary<string, ChatTemplate>()
+        public static Dictionary<string, Type> templates = new Dictionary<string, Type>()
         {
-            {"chatml", new ChatMLTemplate()},
-            {"mistral", new MistralInstructTemplate()},
-            {"llama", new LLama2Template()},
-            {"alpaca", new AlpacaTemplate()},
-            {"phi", new Phi2Template()},
-            {"zephyr", new ZephyrTemplate()},
+            {"chatml", typeof(ChatMLTemplate)},
+            {"mistral (modified for chat)", typeof(MistralChatTemplate)},
+            {"mistral instruct", typeof(MistralInstructTemplate)},
+            {"llama (modified for chat)", typeof(LLama2ChatTemplate)},
+            {"llama", typeof(LLama2Template)},
+            {"alpaca", typeof(AlpacaTemplate)},
+            {"phi", typeof(Phi2Template)},
+            {"zephyr", typeof(ZephyrTemplate)},
         };
 
-        public static Dictionary<string, string> aliasTemplates = new Dictionary<string, string>()
+        public static Dictionary<string, string> modelTemplates = new Dictionary<string, string>()
         {
-            {"openhermes", "chatml"}
+            {"chatml", "chatml"},
+            {"hermes", "chatml"},
+            {"mistral", "mistral (modified for chat)"},
+            {"llama", "llama (modified for chat)"},
+            {"alpaca", "alpaca"},
+            {"phi", "phi"},
+            {"zephyr", "zephyr"},
         };
 
         public static Dictionary<string, string> chatTemplates = new Dictionary<string, string>()
         {
             {"{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}", "chatml"},
-            {"{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token}}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}", "mistral"},
+            {"{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token}}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}", "mistral (modified for chat)"},
             {"{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}", "zephyr"}
         };
 
         public static string FromName(string name)
         {
             string nameLower = name.ToLower();
-            foreach (var pair in aliasTemplates)
+            foreach (var pair in modelTemplates)
             {
                 if (nameLower.Contains(pair.Key)) return pair.Value;
-            }
-            foreach (var pair in templates)
-            {
-                if (nameLower.Contains(pair.Key)) return pair.Key;
             }
             return null;
         }
@@ -85,162 +91,189 @@ namespace LLMUnity
             Debug.Log("No chat template could be matched, fallback to ChatML");
             return "chatml";
         }
-    }
 
-    public class ChatMLTemplate : ChatTemplate
-    {
-        public string RoleString(string role)
+        public static ChatTemplate GetTemplate(string template, string playerName, string AIName)
         {
-            // role as a delimited string for the model
-            return "<|im_start|>" + role + "\n";
+            return (ChatTemplate)Activator.CreateInstance(templates[template], playerName, AIName);
         }
 
-        public string RoleMessageString(string role, string message)
-        {
-            // role and the role message
-            return RoleString(role) + message + "<|im_end|>\n";
-        }
+        public abstract List<string> GetStop();
+        protected virtual string PromptPrefix() { return ""; }
+        protected virtual string SystemPrefix() { return ""; }
+        protected virtual string SystemSuffix() { return ""; }
+        protected virtual string PlayerPrefix() { return ""; }
+        protected virtual string AIPrefix() { return ""; }
+        protected virtual string RequestPrefix() { return ""; }
+        protected virtual string RequestSuffix() { return ""; }
+        protected virtual string PairSuffix() { return ""; }
 
-        public override void SetStop()
+        public virtual string ComputePrompt(List<ChatMessage> messages)
         {
-            stop = new List<string> { "\n<|im_start|>", "<|im_start|>", "\n<|im_end|>", "<|im_end|>"};
-        }
-
-        public override string ComputePrompt(List<ChatMessage> messages)
-        {
-            string chatPrompt = "";
-            for (int i = 0; i < messages.Count; i++)
-            {
-                chatPrompt += RoleMessageString(messages[i].role, messages[i].content);
-            }
-            chatPrompt += RoleString("assistant");
-            return chatPrompt;
-        }
-    }
-
-    public class MistralInstructTemplate : ChatTemplate
-    {
-        public override void SetStop()
-        {
-            stop = new List<string> { "\n[INST]", "[INST]", "\n[/INST]", "[/INST]" };
-        }
-
-        public override string ComputePrompt(List<ChatMessage> messages)
-        {
-            string chatPrompt = "<s>";
+            string chatPrompt = PromptPrefix();
             string systemPrompt = "";
             int start = 0;
             if (messages[0].role == "system")
             {
-                systemPrompt = messages[0].content + "\n\n";
+                systemPrompt = SystemPrefix() + messages[0].content + SystemSuffix();
                 start = 1;
             }
             for (int i = start; i < messages.Count; i += 2)
             {
-                chatPrompt += "[INST] ";
+                chatPrompt += RequestPrefix();
                 if (i == 1 && systemPrompt != "") chatPrompt += systemPrompt;
-                chatPrompt += messages[i].content + " [/INST]";
-                if (i < messages.Count - 1) chatPrompt += messages[i + 1].content + "</s>";
+                if (messages[i].role != playerName)
+                {
+                    Debug.Log($"Role was {messages[i].role}, was expecting {playerName}");
+                }
+                chatPrompt += PlayerPrefix() + messages[i].content + RequestSuffix();
+                if (i < messages.Count - 1)
+                {
+                    if (messages[i + 1].role != AIName)
+                    {
+                        Debug.Log($"Role was {messages[i + 1].role}, was expecting {AIName}");
+                    }
+                    chatPrompt += AIPrefix() + messages[i + 1].content + PairSuffix();
+                }
             }
+            chatPrompt += AIPrefix();
             return chatPrompt;
+        }
+
+        public List<string> AddStopNewlines(List<string> stop)
+        {
+            List<string> stopWithNewLines = new List<string>();
+            foreach (string stopword in stop)
+            {
+                stopWithNewLines.Add(stopword);
+                stopWithNewLines.Add("\n" + stopword);
+            }
+            return stopWithNewLines;
+        }
+    }
+
+    public class ChatMLTemplate : ChatTemplate
+    {
+        public ChatMLTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
+
+        protected override string SystemPrefix() { return "<|im_start|>system\n"; }
+        protected override string SystemSuffix() { return "<|im_end|>\n"; }
+        protected override string PlayerPrefix() { return $"<|im_start|>{playerName}\n"; }
+        protected override string AIPrefix() { return $"<|im_start|>{AIName}\n"; }
+        protected override string RequestSuffix() { return "<|im_end|>\n"; }
+        protected override string PairSuffix() { return "<|im_end|>\n"; }
+
+        public override List<string> GetStop()
+        {
+            return AddStopNewlines(new List<string> { "<|im_start|>", "<|im_end|>" });
         }
     }
 
     public class LLama2Template : ChatTemplate
     {
-        public override void SetStop()
-        {
-            stop = new List<string> { "\n[INST]", "[INST]", "\n[/INST]", "[/INST]" };
-        }
+        public LLama2Template(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
 
-        public override string ComputePrompt(List<ChatMessage> messages)
+        protected override string SystemPrefix() { return "<<SYS>>\n"; }
+        protected override string SystemSuffix() { return "\n<</SYS>> "; }
+        protected override string RequestPrefix() { return "<s>[INST] "; }
+        protected override string RequestSuffix() { return " [/INST]"; }
+        protected override string PairSuffix() { return " </s>"; }
+
+        public override List<string> GetStop()
         {
-            string chatPrompt = "";
-            string systemPrompt = "";
-            int start = 0;
-            if (messages[0].role == "system")
-            {
-                systemPrompt = "<<SYS>>\n" + messages[0].content + "\n<</SYS>> ";
-                start = 1;
-            }
-            for (int i = start; i < messages.Count; i += 2)
-            {
-                chatPrompt += "<s>[INST] ";
-                if (i == 1 && systemPrompt != "") chatPrompt += systemPrompt;
-                chatPrompt += messages[i].content + " [/INST]";
-                if (i < messages.Count - 1) chatPrompt += messages[i + 1].content + " </s>";
-            }
-            return chatPrompt;
+            return AddStopNewlines(new List<string> { "[INST]", "[/INST]" });
+        }
+    }
+
+    public class LLama2ChatTemplate : LLama2Template
+    {
+        public LLama2ChatTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
+
+        protected override string PlayerPrefix() { return "### " + playerName + ": "; }
+        protected override string AIPrefix() { return "### " + AIName + ": "; }
+
+        public override List<string> GetStop()
+        {
+            return AddStopNewlines(new List<string> { "[INST]", "[/INST]", "###" });
+        }
+    }
+
+    public class MistralInstructTemplate : ChatTemplate
+    {
+        public MistralInstructTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
+
+        protected override string PromptPrefix() { return "<s>"; }
+        protected override string SystemPrefix() { return ""; }
+        protected override string SystemSuffix() { return "\n\n"; }
+        protected override string RequestPrefix() { return "[INST] "; }
+        protected override string RequestSuffix() { return " [/INST]"; }
+        protected override string PairSuffix() { return "</s>"; }
+
+        public override List<string> GetStop()
+        {
+            return AddStopNewlines(new List<string> { "[INST]", "[/INST]" });
+        }
+    }
+
+    public class MistralChatTemplate : MistralInstructTemplate
+    {
+        public MistralChatTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
+
+        protected override string PlayerPrefix() { return "### " + playerName + ": "; }
+        protected override string AIPrefix() { return "### " + AIName + ": "; }
+
+        public override List<string> GetStop()
+        {
+            return AddStopNewlines(new List<string> { "[INST]", "[/INST]", "###" });
         }
     }
 
     public class AlpacaTemplate : ChatTemplate
     {
-        public override void SetStop()
-        {
-            stop = new List<string> { "\n###", "###" };
-        }
+        public AlpacaTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
 
-        public override string ComputePrompt(List<ChatMessage> messages)
+        protected override string SystemSuffix() { return "\n\n"; }
+        protected override string RequestSuffix() { return "\n\n"; }
+        protected override string PlayerPrefix() { return "### " + playerName + ": "; }
+        protected override string AIPrefix() { return "### " + AIName + ": "; }
+        protected override string PairSuffix() { return "</s>"; }
+
+        public override List<string> GetStop()
         {
-            string chatPrompt = "";
-            int start = 0;
-            if (messages[0].role == "system")
-            {
-                chatPrompt += messages[0].content + "\n\n";
-                start = 1;
-            }
-            for (int i = start; i < messages.Count; i += 2)
-            {
-                chatPrompt += "### Instruction: " + messages[i].content + "\n\n";
-                if (i < messages.Count - 1) chatPrompt += "### Response: " + messages[i + 1].content + "</s>";
-            }
-            return chatPrompt;
+            return AddStopNewlines(new List<string> { "###" });
         }
     }
 
+
     public class Phi2Template : ChatTemplate
     {
-        public override void SetStop()
-        {
-            stop = new List<string> { "\nuser:", "user:", "\nassistant:", "assistant:"};
-        }
+        public Phi2Template(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
 
-        public override string ComputePrompt(List<ChatMessage> messages)
+        protected override string SystemSuffix() { return "\n\n"; }
+        protected override string RequestSuffix() { return "\n"; }
+        protected override string PlayerPrefix() { return playerName + ": "; }
+        protected override string AIPrefix() { return AIName + ": "; }
+        protected override string PairSuffix() { return "\n"; }
+
+        public override List<string> GetStop()
         {
-            string chatPrompt = "";
-            int start = 0;
-            if (messages[0].role == "system")
-            {
-                chatPrompt += messages[0].content + "\n\n";
-                start = 1;
-            }
-            for (int i = start; i < messages.Count; i += 2)
-            {
-                chatPrompt += "user: " + messages[i].content + "\n";
-                if (i < messages.Count - 1) chatPrompt += "assistant: " + messages[i + 1].content + "\n";
-            }
-            chatPrompt += "assistant: ";
-            return chatPrompt;
+            return AddStopNewlines(new List<string> { playerName + ":", AIName + ":" });
         }
     }
 
     public class ZephyrTemplate : ChatTemplate
     {
-        public override void SetStop()
-        {
-            stop = new List<string> { "\n<|user|>", "<|user|>", "\n<|assistant|>", "<|assistant|>"};
-        }
+        public ZephyrTemplate(string playerName = "user", string AIName = "assistant") : base(playerName, AIName) {}
 
-        public override string ComputePrompt(List<ChatMessage> messages)
+        protected override string SystemPrefix() { return "<|system|>\n"; }
+        protected override string SystemSuffix() { return "</s>\n"; }
+        protected override string PlayerPrefix() { return $"<|{playerName}|>\n"; }
+        protected override string AIPrefix() { return $"<|{AIName}|>\n"; }
+        protected override string RequestSuffix() { return "</s>\n"; }
+        protected override string PairSuffix() { return "</s>\n"; }
+
+        public override List<string> GetStop()
         {
-            string chatPrompt = "";
-            for (int i = 0; i < messages.Count; i++)
-            {
-                chatPrompt += "<|" + messages[i].role + "|>\n" + messages[i].content + "</s>\n";
-            }
-            chatPrompt += "<|assistant|>\n";
-            return chatPrompt;
+            return AddStopNewlines(new List<string> { $"<|{playerName}|>", $"<|{AIName}|>" });
         }
     }
 }
