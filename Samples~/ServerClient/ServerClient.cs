0b3dc1650169749e36177c1dc51b14849c1e2289
Antonis Makropoulos <benuix@gmail.com>
2024-03-07 14:45:49 +0200

    use namespaces in LLMGGUF and samples

--- Samples~/ServerClient/ServerClient.cs
+++ Samples~/ServerClient/ServerClient.cs
@@ -3,76 +3,79 @@ using LLMUnity;
 using UnityEngine.UI;
 
 
-public class ServerClientInteraction
+namespace LLMUnitySamples
 {
-    InputField playerText;
-    Text AIText;
-    LLMClient llm;
-
-    public ServerClientInteraction(InputField playerText, Text AIText, LLMClient llm)
+    public class ServerClientInteraction
     {
-        this.playerText = playerText;
-        this.AIText = AIText;
-        this.llm = llm;
+        InputField playerText;
+        Text AIText;
+        LLMClient llm;
+
+        public ServerClientInteraction(InputField playerText, Text AIText, LLMClient llm)
+        {
+            this.playerText = playerText;
+            this.AIText = AIText;
+            this.llm = llm;
+        }
+
+        public void Start()
+        {
+            playerText.onSubmit.AddListener(onInputFieldSubmit);
+            playerText.Select();
+        }
+
+        public void onInputFieldSubmit(string message)
+        {
+            playerText.interactable = false;
+            AIText.text = "...";
+            _ = llm.Chat(message, SetAIText, AIReplyComplete);
+        }
+
+        public void SetAIText(string text)
+        {
+            AIText.text = text;
+        }
+
+        public void AIReplyComplete()
+        {
+            playerText.interactable = true;
+            playerText.Select();
+            playerText.text = "";
+        }
     }
 
-    public void Start()
+    public class ServerClient : MonoBehaviour
     {
-        playerText.onSubmit.AddListener(onInputFieldSubmit);
-        playerText.Select();
-    }
+        public LLM llm;
+        public InputField playerText1;
+        public Text AIText1;
+        ServerClientInteraction interaction1;
 
-    public void onInputFieldSubmit(string message)
-    {
-        playerText.interactable = false;
-        AIText.text = "...";
-        _ = llm.Chat(message, SetAIText, AIReplyComplete);
-    }
+        public LLMClient llmClient;
+        public InputField playerText2;
+        public Text AIText2;
+        ServerClientInteraction interaction2;
 
-    public void SetAIText(string text)
-    {
-        AIText.text = text;
-    }
+        void Start()
+        {
+            interaction1 = new ServerClientInteraction(playerText1, AIText1, llm);
+            interaction2 = new ServerClientInteraction(playerText2, AIText2, llmClient);
+            interaction1.Start();
+            interaction2.Start();
+        }
 
-    public void AIReplyComplete()
-    {
-        playerText.interactable = true;
-        playerText.Select();
-        playerText.text = "";
-    }
-}
+        public void CancelRequests()
+        {
+            llm.CancelRequests();
+            llmClient.CancelRequests();
+            interaction1.AIReplyComplete();
+            interaction2.AIReplyComplete();
+        }
 
-public class ServerClient : MonoBehaviour
-{
-    public LLM llm;
-    public InputField playerText1;
-    public Text AIText1;
-    ServerClientInteraction interaction1;
-
-    public LLMClient llmClient;
-    public InputField playerText2;
-    public Text AIText2;
-    ServerClientInteraction interaction2;
-
-    void Start()
-    {
-        interaction1 = new ServerClientInteraction(playerText1, AIText1, llm);
-        interaction2 = new ServerClientInteraction(playerText2, AIText2, llmClient);
-        interaction1.Start();
-        interaction2.Start();
-    }
-
-    public void CancelRequests()
-    {
-        llm.CancelRequests();
-        llmClient.CancelRequests();
-        interaction1.AIReplyComplete();
-        interaction2.AIReplyComplete();
-    }
-
-    public void ExitGame()
-    {
-        Debug.Log("Exit button clicked");
-        Application.Quit();
+        public void ExitGame()
+        {
+            Debug.Log("Exit button clicked");
+            Application.Quit();
+        }
     }
 }
