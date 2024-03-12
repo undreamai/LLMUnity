49123690157de8aec8463070aa98edc8cf582a8d
Antonis Makropoulos <benuix@gmail.com>
2024-03-12 13:04:45 +0200

    use an actual model in unit tests

--- Tests/Runtime/TestLLM.cs
+++ Tests/Runtime/TestLLM.cs
@@ -40,9 +40,8 @@ namespace LLMUnityTests
         GameObject gameObject;
         LLMNoAwake llm;
         int port = 15555;
-        string AIReply = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         Exception error = null;
-        string prompt = "You";
+        string prompt = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.";
 
         public TestLLM()
         {
@@ -56,14 +55,16 @@ namespace LLMUnityTests
             gameObject.SetActive(false);
 
             llm = gameObject.AddComponent<LLMNoAwake>();
-
-            string modelUrl = "https://huggingface.co/aladar/tiny-random-BloomForCausalLM-GGUF/resolve/main/tiny-random-BloomForCausalLM.gguf?download=true";
-            string modelPath = "LLMUnityTests/tiny-random-BloomForCausalLM.gguf";
+            string modelUrl = "https://huggingface.co/afrideva/smol_llama-220M-openhermes-GGUF/resolve/main/smol_llama-220m-openhermes.q4_k_m.gguf?download=true";
+            string modelPath = "LLMUnityTests/smol_llama-220m-openhermes.q4_k_m.gguf";
             string fullModelPath = LLMUnitySetup.GetAssetPath(modelPath);
             _ = LLMUnitySetup.DownloadFile(modelUrl, fullModelPath, false, false, null, null, false);
             await llm.SetModel(fullModelPath);
             Assert.AreEqual(llm.model, modelPath);
 
+            llm.port = port;
+            llm.playerName = "Instruction";
+            llm.AIName = "Response";
             llm.port = port;
             llm.prompt = prompt;
             llm.temperature = 0;
@@ -71,6 +72,7 @@ namespace LLMUnityTests
             llm.stream = false;
             llm.numPredict = 20;
             llm.parallelPrompts = 1;
+            llm.SetTemplate("alpaca");
 
             gameObject.SetActive(true);
         }
@@ -86,9 +88,9 @@ namespace LLMUnityTests
                 await llm.Warmup();
                 TestInitParameters((await llm.Tokenize(prompt)).Count, 1);
                 TestWarmup();
-                await llm.Chat("hi", TestChat);
+                await llm.Chat("How can I increase my meme production/output? Currently, I only create them in ancient babylonian which is time consuming.", TestChat);
                 TestPostChat();
-                prompt = "You are";
+                prompt = "How are you?";
                 llm.SetPrompt(prompt);
                 await llm.Chat("hi");
                 TestInitParameters((await llm.Tokenize(prompt)).Count, 3);
@@ -126,7 +128,7 @@ namespace LLMUnityTests
 
         public void TestTokens(List<int> tokens)
         {
-            Assert.AreEqual(tokens, new List<int> {44});
+            Assert.AreEqual(tokens, new List<int> {306});
         }
 
         public void TestWarmup()
@@ -136,7 +138,8 @@ namespace LLMUnityTests
 
         public void TestChat(string reply)
         {
-            Assert.That(reply == AIReply);
+            string AIReply = "One way to increase your meme production/output is by creating a more complex and customized";
+            Assert.That(reply.Trim() == AIReply);
         }
 
         public void TestPostChat()
