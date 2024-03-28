9a743c1646fc8087b2fd70826e8af9b1f7ca7b74
Antonis Makropoulos <benuix@gmail.com>
2024-03-28 15:02:46 +0200

    document LLMClient public variables

--- Runtime/LLMClient.cs
+++ Runtime/LLMClient.cs
@@ -1,3 +1,4 @@
+/// @file
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using UnityEngine.Networking;
 
 namespace LLMUnity
 {
+    /// \cond HIDE_CLASS
     public sealed class FloatAttribute : PropertyAttribute
     {
         public float Min { get; private set; }
@@ -39,59 +41,110 @@ namespace LLMUnity
     public class ModelAdvancedAttribute : PropertyAttribute {}
     public class ModelAddonAdvancedAttribute : PropertyAttribute {}
     public class ModelExpertAttribute : PropertyAttribute {}
+    /// \endcond
 
     [DefaultExecutionOrder(-1)]
     public class LLMClient : MonoBehaviour
     {
+        /// <summary> toggle to show/hide advanced options in the GameObject </summary>
         [HideInInspector] public bool advancedOptions = false;
+        /// <summary> toggle to show/hide expert options in the GameObject </summary>
         [HideInInspector] public bool expertOptions = false;
 
+        /// <summary> host to use for the LLMClient object </summary>
         [ClientAdvanced] public string host = "localhost";
+        /// <summary> port to use for the server (LLM) or client (LLMClient) </summary>
         [ServerAdvanced] public int port = 13333;
+        /// <summary> option to receive the reply from the model as it is produced (recommended!).
+        /// If it is not selected, the full reply from the model is received in one go </summary>
         [Server] public bool stream = true;
 
+        /// <summary> grammar file used for the LLM in .cbnf format </summary>
         [ModelAddonAdvanced] public string grammar = null;
+        /// <summary> seed for reproducibility. For random results every time set to -1. </summary>
         [ModelAdvanced] public int seed = 0;
+        /// <summary> number of tokens to predict (-1 = infinity, -2 = until context filled).
+        /// This is the amount of tokens the model will maximum predict.
+        /// When N predict is reached the model will stop generating.
+        /// This means words / sentences might not get finished if this is too low. </summary>
         [ModelAdvanced] public int numPredict = 256;
+        /// <summary> option to cache the prompt as it is being created by the chat to avoid reprocessing the entire prompt every time (default: true) </summary>
         [ModelAdvanced] public bool cachePrompt = true;
+        /// <summary> LLM temperature, lower values give more deterministic answers.
+        /// The temperature setting adjusts how random the generated responses are.
+        /// Turning it up makes the generated choices more varied and unpredictable.
+        /// Turning it down makes the generated responses more predictable and focused on the most likely options. </summary>
         [ModelAdvanced, Float(0f, 2f)] public float temperature = 0.2f;
+        /// <summary> top-k sampling (0 = disabled)
+        /// The top k value controls the top k most probable tokens at each step of generation. This value can help fine tune the output and make this adhere to specific patterns or constraints. </summary>
         [ModelAdvanced, Int(-1, 100)] public int topK = 40;
+        /// <summary> top-p sampling (1.0 = disabled)
+        /// The top p value controls the cumulative probability of generated tokens.
+        /// The model will generate tokens until this theshold (p) is reached.
+        /// By lowering this value you can shorten output & encourage / discourage more diverse output. </summary>
         [ModelAdvanced, Float(0f, 1f)] public float topP = 0.9f;
+        /// <summary> minimum probability for a token to be used.
+        /// The probability is defined relative to the probability of the most likely token. </summary>
         [ModelAdvanced, Float(0f, 1f)] public float minP = 0.05f;
+        /// <summary> control the repetition of token sequences in the generated text.
+        /// The penalty is applied to repeated tokens. </summary>
         [ModelAdvanced, Float(0f, 1f)] public float repeatPenalty = 1.1f;
+        /// <summary> repeated token presence penalty (0.0 = disabled)
+        /// Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics. </summary>
         [ModelAdvanced, Float(0f, 1f)] public float presencePenalty = 0f;
+        /// <summary> repeated token frequency penalty (0.0 = disabled)
+        /// Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim. </summary>
         [ModelAdvanced, Float(0f, 1f)] public float frequencyPenalty = 0f;
 
+        /// <summary> enable tail free sampling with parameter z (1.0 = disabled). </summary>
         [ModelExpert, Float(0f, 1f)] public float tfsZ = 1f;
+        /// <summary> enable locally typical sampling with parameter p (1.0 = disabled). </summary>
         [ModelExpert, Float(0f, 1f)] public float typicalP = 1f;
+        /// <summary> last n tokens to consider for penalizing repetition (0 = disabled, -1 = ctx-size). </summary>
         [ModelExpert, Int(0, 2048)] public int repeatLastN = 64;
+        /// <summary> penalize newline tokens when applying the repeat penalty . </summary>
         [ModelExpert] public bool penalizeNl = true;
+        /// <summary> prompt for the purpose of the penalty evaluation.
+        /// Can be either null, a string or an array of numbers representing tokens (null/"" = use original prompt) </summary>
         [ModelExpert] public string penaltyPrompt;
+        /// <summary> enable Mirostat sampling, controlling perplexity during text generation (0 = disabled, 1 = Mirostat, 2 = Mirostat 2.0). </summary>
         [ModelExpert, Int(0, 2)] public int mirostat = 0;
+        /// <summary> set the Mirostat target entropy, parameter tau. </summary>
         [ModelExpert, Float(0f, 10f)] public float mirostatTau = 5f;
+        /// <summary> set the Mirostat learning rate, parameter eta. </summary>
         [ModelExpert, Float(0f, 1f)] public float mirostatEta = 0.1f;
+        /// <summary> if greater than 0, the response also contains the probabilities of top N tokens for each generated token </summary>
         [ModelExpert, Int(0, 10)] public int nProbs = 0;
+        /// <summary> ignore end of stream token and continue generating. </summary>
         [ModelExpert] public bool ignoreEos = false;
 
+        /// <summary> number of tokens to retain from the prompt when the model runs out of context (-1 = LLM/LLMClient prompt tokens if setNKeepToPrompt is set to true). </summary>
         public int nKeep = -1;
+        /// <summary> stopwords to stop the LLM in addition to the default stopwords from the chat template. </summary>
         public List<string> stop = new List<string>();
+        /// <summary> the logit bias option allows to manually adjust the likelihood of specific tokens appearing in the generated text.
+        /// By providing a token ID and a positive or negative bias value, you can increase or decrease the probability of that token being generated. </summary>
         public Dictionary<int, string> logitBias = null;
-        public string grammarString;
 
+        /// <summary> the name of the player </summary>
         [Chat] public string playerName = "user";
+        /// <summary> the name of the AI </summary>
         [Chat] public string AIName = "assistant";
+        /// <summary> a description of the AI role. This defines the LLM/LLMClient system prompt </summary>
         [TextArea(5, 10), Chat] public string prompt = "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions.";
+        /// <summary> option to set the number of tokens to retain from the prompt (nKeep) based on the LLM/LLMClient (system) prompt </summary>
+        public bool setNKeepToPrompt = true;
 
         protected List<ChatMessage> chat;
-        public string chatTemplate = ChatTemplate.DefaultTemplate;
-        public ChatTemplate template;
+        private LLM server;
         private List<(string, string)> requestHeaders = new List<(string, string)> { ("Content-Type", "application/json") };
         private string previousEndpoint;
-        public bool setNKeepToPrompt = true;
         private List<UnityWebRequest> WIPRequests = new List<UnityWebRequest>();
         static object chatPromptLock = new object();
         static object chatAddLock = new object();
-        private LLM server;
+        public string chatTemplate = ChatTemplate.DefaultTemplate;
+        public ChatTemplate template;
+        public string grammarString;
 
         public void Awake()
         {
