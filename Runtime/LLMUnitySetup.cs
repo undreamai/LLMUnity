1de4572ffa75d5de0644308a7438677b0536a467
amakropoulos <amakropoulos@users.noreply.github.com>
2024-05-28 08:50:09 +0000

    update VERSION

--- Runtime/LLMUnitySetup.cs
+++ Runtime/LLMUnitySetup.cs
@@ -32,7 +32,7 @@ namespace LLMUnity
     {
         // DON'T CHANGE! the version is autocompleted with a GitHub action
         /// <summary> LLM for Unity version </summary>
-        public static string Version = "v1.2.8";
+        public static string Version = "v1.2.9";
 
         /// \cond HIDE
         public static Process CreateProcess(
