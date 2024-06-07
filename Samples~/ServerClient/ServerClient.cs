0eef10c360e0c0224f36fce6537cffb240b9b88b
Antonis Makropoulos <benuix@gmail.com>
2024-06-07 21:32:51 +0300

    adapt samples

--- Samples~/ServerClient/ServerClient.cs
+++ Samples~/ServerClient/ServerClient.cs
@@ -47,27 +47,29 @@ namespace LLMUnitySamples
     public class ServerClient : MonoBehaviour
     {
         public LLM llm;
+
+        public LLMClient llmClient1;
         public InputField playerText1;
         public Text AIText1;
         ServerClientInteraction interaction1;
 
-        public LLMClient llmClient;
+        public LLMClient llmClient2;
         public InputField playerText2;
         public Text AIText2;
         ServerClientInteraction interaction2;
 
         void Start()
         {
-            interaction1 = new ServerClientInteraction(playerText1, AIText1, llm);
-            interaction2 = new ServerClientInteraction(playerText2, AIText2, llmClient);
+            interaction1 = new ServerClientInteraction(playerText1, AIText1, llmClient1);
+            interaction2 = new ServerClientInteraction(playerText2, AIText2, llmClient2);
             interaction1.Start();
             interaction2.Start();
         }
 
         public void CancelRequests()
         {
-            llm.CancelRequests();
-            llmClient.CancelRequests();
+            llmClient1.CancelRequests();
+            llmClient2.CancelRequests();
             interaction1.AIReplyComplete();
             interaction2.AIReplyComplete();
         }
