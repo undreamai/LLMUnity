4380c1c691c10586223338f6ea290af7c4d5a865
amakropoulos <amakropoulos@users.noreply.github.com>
2024-07-11 17:35:58 +0000

    update VERSION

--- Runtime/LLMUnitySetup.cs
+++ Runtime/LLMUnitySetup.cs
@@ -68,7 +68,7 @@ namespace LLMUnity
     {
         // DON'T CHANGE! the version is autocompleted with a GitHub action
         /// <summary> LLM for Unity version </summary>
-        public static string Version = "v2.0.2";
+        public static string Version = "v2.0.3";
         /// <summary> LlamaLib version </summary>
         public static string LlamaLibVersion = "v1.1.5";
         /// <summary> LlamaLib url </summary>
