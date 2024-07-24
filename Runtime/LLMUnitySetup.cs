fa6a2afa0dc6e9ef80c9f4bdb2c6198fc5f240f9
Antonis Makropoulos <benuix@gmail.com>
2024-07-24 15:54:29 +0300

    add custom url option

--- Runtime/LLMUnitySetup.cs
+++ Runtime/LLMUnitySetup.cs
@@ -87,6 +87,7 @@ namespace LLMUnity
         [HideInInspector] public static readonly (string, string)[] modelOptions = new(string, string)[]
         {
             ("Download model", null),
+            ("Custom URL", null),
             ("Mistral 7B Instruct v0.2 (medium, best overall)", "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf?download=true"),
             ("OpenHermes 2.5 7B (medium, best for conversation)", "https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/resolve/main/openhermes-2.5-mistral-7b.Q4_K_M.gguf?download=true"),
             ("Phi 3 (small, great)", "https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-gguf/resolve/main/Phi-3-mini-4k-instruct-q4.gguf?download=true"),
