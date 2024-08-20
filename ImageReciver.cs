5fd70b41daed571a2764c1528f61f5b1a7da7c2c
NOSCOPEdev <114004267+NOSCOPEdev@users.noreply.github.com>
2024-08-20 17:36:12 +0300

    LastFix.

--- ImageReciver.cs
+++ ImageReciver.cs
@@ -5,13 +5,14 @@ using System.Text;
 using Newtonsoft.Json.Linq;
 using LLMUnity;
 using LLMUnitySamples;
-
+using TMPro;
 public class ImageReciver : MonoBehaviour
 {
     //Noskop_dev was here :D
 
     //This field is used to relay the image to the AI, this can be done by both a URL or a file in your system.
-    public string AnyImageData;
+   
+    public TMP_InputField AnyImageData;
 
     // Should work with any script that calls the Chat function on the LLMCharacter script. 
     public AndroidDemo AD;
@@ -30,7 +31,7 @@ public class ImageReciver : MonoBehaviour
     
     public void SendImageToAI()  
     {
-        AD.onInputFieldSubmit(" [\r\n        {\"role\": \"system\", \"content\": \"You are an assistant who perfectly describes images.\"},\r\n        {\r\n            \"role\": \"user\",\r\n            \"content\": [\r\n                {\"type\" : \"text\", \"text\": \"What's in this image?\"},\r\n                {\"type\": \"image_url\", \"image_url\": {\"url\":" + AnyImageData + "\" } }\r\n            ]");
+        AD.onInputFieldSubmit(" [\r\n        {\"role\": \"system\", \"content\": \"You are an assistant who perfectly describes images.\"},\r\n        {\r\n            \"role\": \"user\",\r\n            \"content\": [\r\n                {\"type\" : \"text\", \"text\": \"What's in this image?\"},\r\n                {\"type\": \"image_url\", \"image_url\": {\"url\":" + AnyImageData.text + "\" } }\r\n            ]");
 
        
     }
