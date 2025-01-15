f642d38ac0236c173050263546882bfaa2ae41b7
Antonis Makropoulos <benuix@gmail.com>
2025-01-15 09:35:05 +0200

    bump LlamaLib to v2.4.2

--- Runtime/LLMUnitySetup.cs
+++ Runtime/LLMUnitySetup.cs
@@ -103,7 +103,7 @@ namespace LLMUnity
         /// <summary> LLM for Unity version </summary>
         public static string Version = "v2.4.1";
         /// <summary> LlamaLib version </summary>
-        public static string LlamaLibVersion = "v1.2.1";
+        public static string LlamaLibVersion = "v1.2.2";
         /// <summary> LlamaLib release url </summary>
         public static string LlamaLibReleaseURL = $"https://github.com/undreamai/LlamaLib/releases/download/{LlamaLibVersion}";
         /// <summary> LlamaLib name </summary>
