9250efd487aaab9381888c780a143d3c3b23b731
Antonis Makropoulos <benuix@gmail.com>
2025-12-02 14:39:34 +0200

    remove save cache functionality

--- Runtime/LLMCharacter.cs
+++ Runtime/LLMCharacter.cs
@@ -28,7 +28,7 @@ namespace LLMUnity
         public void SetPrompt(string newPrompt, bool clearChat = true)
         {
             systemPrompt = newPrompt;
-            if (clearChat) _ = ClearChat();
+            if (clearChat) _ = ClearHistory();
         }
     }
 }
