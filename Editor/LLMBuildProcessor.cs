f08e89a4494ed219dbfc97ec50f9d2da7b07f1ef
Antonis Makropoulos <benuix@gmail.com>
2025-12-05 13:28:46 +0200

    Add undream libraries in macOS Xcode

--- Editor/LLMBuildProcessor.cs
+++ Editor/LLMBuildProcessor.cs
@@ -2,7 +2,8 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
-#if UNITY_IOS || UNITY_VISIONOS
+using System.Collections.Generic;
+#if UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_VISIONOS
 using System.IO;
 using UnityEditor.iOS.Xcode;
 #endif
@@ -27,48 +28,75 @@ namespace LLMUnity
             if (type == LogType.Error) BuildCompleted();
         }
 
-#if UNITY_IOS || UNITY_VISIONOS
+#if UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_VISIONOS
         /// <summary>
         /// Postprocess the iOS Build
         /// </summary>
         public static void PostprocessIOSBuild(BuildTarget buildTarget, string outputPath)
         {
+            List<string> libraryFileNames = new List<string>();
+#if UNITY_IOS
             string projPath = PBXProject.GetPBXProjectPath(outputPath);
-#if UNITY_VISIONOS
-            projPath = projPath.Replace("Unity-iPhone", "Unity-VisionOS");
+            libraryFileNames.Add("libllamalib_ios-arm64.a");
+#elif UNITY_VISIONOS
+            string projPath = PBXProject.GetPBXProjectPath(outputPath).Replace("Unity-iPhone", "Unity-VisionOS");
+            libraryFileNames.Add("libllamalib_visionos-arm64.a");
+#else
+            string projPath = Path.Combine(outputPath, Path.GetFileName(outputPath) + ".xcodeproj", "project.pbxproj");
+            if (!File.Exists(projPath)) return;
+            libraryFileNames.Add("libllamalib_osx-universal_acc.dylib");
+            libraryFileNames.Add("libllamalib_osx-universal_no-acc.dylib");
 #endif
+
             PBXProject project = new PBXProject();
             project.ReadFromFile(projPath);
 
-            string targetGuid = project.GetUnityFrameworkTargetGuid();
-            string frameworkTargetGuid = project.GetUnityFrameworkTargetGuid();
             string unityMainTargetGuid = project.GetUnityMainTargetGuid();
-            string embedFrameworksGuid = project.GetResourcesBuildPhaseByTarget(frameworkTargetGuid);
+            string targetGuid = project.GetUnityFrameworkTargetGuid();
 
             // Add Accelerate framework
             project.AddFrameworkToProject(unityMainTargetGuid, "Accelerate.framework", false);
-            project.AddFrameworkToProject(targetGuid, "Accelerate.framework", false);
+            if (targetGuid != null) project.AddFrameworkToProject(targetGuid, "Accelerate.framework", false);
 
-            string libraryFile = LLMUnitySetup.RelativePath(LLMUnitySetup.SearchDirectory(outputPath, $"libundreamai_{buildTarget.ToString().ToLower()}.a"), outputPath);
-            string fileGuid = project.FindFileGuidByProjectPath(libraryFile);
-            if (string.IsNullOrEmpty(fileGuid)) LLMUnitySetup.LogError($"Library file {libraryFile} not found in project");
+            List<string> libraryFiles = new List<string>();
+            foreach (string libraryFileName in libraryFileNames)
+            {
+                string lib = LLMUnitySetup.SearchDirectory(outputPath, libraryFileName);
+                if (lib != null) libraryFiles.Add(lib);
+            }
+
+            if (libraryFiles.Count == 0)
+            {
+                Debug.LogError($"No library files found for the build");
+            }
             else
             {
-                foreach (var phaseGuid in project.GetAllBuildPhasesForTarget(unityMainTargetGuid))
+                foreach (string libraryFile in libraryFiles)
                 {
-                    if (project.GetBuildPhaseName(phaseGuid) == "Embed Frameworks")
+                    string relLibraryFile = LLMUnitySetup.RelativePath(libraryFile, outputPath);
+                    string fileGuid = project.FindFileGuidByProjectPath(relLibraryFile);
+                    if (string.IsNullOrEmpty(fileGuid))
                     {
-                        project.RemoveFileFromBuild(phaseGuid, fileGuid);
-                        break;
+                        Debug.LogError($"Library file {relLibraryFile} not found in project");
+                    }
+                    else
+                    {
+                        foreach (var phaseGuid in project.GetAllBuildPhasesForTarget(unityMainTargetGuid))
+                        {
+                            if (project.GetBuildPhaseName(phaseGuid) == "Embed Frameworks")
+                            {
+                                project.RemoveFileFromBuild(phaseGuid, fileGuid);
+                                break;
+                            }
+                        }
+
+                        project.AddFileToBuild(unityMainTargetGuid, fileGuid);
+                        if (targetGuid != null) project.AddFileToBuild(targetGuid, fileGuid);
                     }
                 }
-
-                project.AddFileToBuild(unityMainTargetGuid, fileGuid);
-                project.AddFileToBuild(targetGuid, fileGuid);
             }
 
             project.WriteToFile(projPath);
-            AssetDatabase.ImportAsset(projPath);
         }
 
 #endif
@@ -76,7 +104,7 @@ namespace LLMUnity
         // called after the build
         public void OnPostprocessBuild(BuildReport report)
         {
-#if UNITY_IOS || UNITY_VISIONOS
+#if UNITY_STANDALONE_OSX || UNITY_IOS || UNITY_VISIONOS
             PostprocessIOSBuild(report.summary.platform, report.summary.outputPath);
 #endif
             EditorApplication.delayCall += () =>
